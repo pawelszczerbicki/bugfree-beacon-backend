8f498e83ae0971e279b4edb13eb69a262f114ddb
shark <szczerbicki.pawel@gmail.com>
2015-02-17 00:10:42 +0100

    chamge default database, change log level

--- config/connections.js
+++ config/connections.js
@@ -1,13 +1,4 @@
 module.exports.connections = {
-
-  /***************************************************************************
-  *                                                                          *
-  * MongoDB is the leading NoSQL database.                                   *
-  * http://en.wikipedia.org/wiki/MongoDB                                     *
-  *                                                                          *
-  * Run: npm install sails-mongo                                             *
-  *                                                                          *
-  ***************************************************************************/
   localMongoDatabase: {
     adapter: 'sails-mongo',
     host: 'localhost',

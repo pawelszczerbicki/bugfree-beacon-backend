0e95a2d4eff8a0d77e1965e05a4d723026bd09ab
Norbert <khozzy@gmail.com>
2015-02-26 20:20:38 +0100

    Script publishing the data

--- scripts/prepareDB.sh
+++ scripts/prepareDB.sh
@@ -1,4 +1,4 @@
-curl -XPOST -H "Content-type: application/json" localhost:1337/customer/ -d '
+curl -XPOST -H "Content-type: application/json" bejkon.herokuapp.com/customer/ -d '
 {
   "name": "Mariolka Fryzjer",
   "description": "Fryzjer Mariolka najlepszy na świecie. Idzicie tylko tutaj",
@@ -10,7 +10,7 @@ curl -XPOST -H "Content-type: application/json" localhost:1337/customer/ -d '
 }
 '
 
-curl -XPOST -H "Content-type: application/json" localhost:1337/customer/ -d '
+curl -XPOST -H "Content-type: application/json" http://bejkon.herokuapp.com/customer/ -d '
 {
   "name": "BIGBUT",
   "description": "W BIGBUT kupisz drogo naprawdę super duże buty",
@@ -22,36 +22,36 @@ curl -XPOST -H "Content-type: application/json" localhost:1337/customer/ -d '
 }
 '
 
-curl -XPOST -H "Content-type: application/json" localhost:1337/beacon -d '
+curl -XPOST -H "Content-type: application/json" http://bejkon.herokuapp.com/beacon -d '
 {
   "uuid": "B9407F30-F5F8-466E-AFF9-25556B57FE6D",
   "major": 36077,
   "minor": 12917,
-  "customer": "54eea176504df2cb0380cb26",
+  "customer": 1,
   "name": "Witryna",
   "image_url": "http://img01.imgsinemalar.com/images/group_image_buyuk/8329/Kel-AdamJohnny-Sins-1.jpg",
   "push_text" : "Strzyżenie na łyso za 4 zł!"
 }
 '
 
-curl -XPOST -H "Content-type: application/json" localhost:1337/beacon -d '
+curl -XPOST -H "Content-type: application/json" http://bejkon.herokuapp.com/beacon -d '
 {
   "uuid": "B9407F30-F5F8-466E-AFF9-25556B57FE6D",
   "major": 58599,
   "minor": 39914,
-  "customer": "54eea22f504df2cb0380cb27",
+  "customer": 2,
   "name": "Witryna",
   "image_url": "http://cdn.frontpagemag.com/wp-content/uploads/2013/03/0-big_shoe.jpg",
   "push_text" : "Rabat 20% z tą notyfikacją"
 }
 '
 
-curl -XPOST -H "Content-type: application/json" localhost:1337/beacon -d '
+curl -XPOST -H "Content-type: application/json" http://bejkon.herokuapp.com/beacon -d '
 {
   "uuid": "B9407F30-F5F8-466E-AFF9-25556B57FE6D",
   "major": 37751,
   "minor": 33470,
-  "customer": "54eea22f504df2cb0380cb27",
+  "customer": 2,
   "name": "Kasa",
   "image_url": "https://janetnewenham.files.wordpress.com/2013/09/800px-big-shoe-tricycles-vehicle.jpg",
   "push_text" : "Dziękujemy za zakupy. Zobacz na fajny obrazek :)"

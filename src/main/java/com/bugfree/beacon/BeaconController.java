03e3ddb9fa0750117f8dc9abf8e168c34db71ca2
Szczerbicki <pawel.szczerbicki@crazycall.com>
2015-03-26 15:27:44 +0100

    fix import problem

--- src/main/java/com/bugfree/beacon/BeaconController.java
+++ src/main/java/com/bugfree/beacon/BeaconController.java
@@ -5,6 +5,7 @@ import com.bugfree.json.JsonResponse;
 import com.bugfree.json.SuccessResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;

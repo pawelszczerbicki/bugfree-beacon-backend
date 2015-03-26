aba7ea6fff6e503bb0cccce2489e4f6dac43707a
Szczerbicki <pawel.szczerbicki@crazycall.com>
2015-03-26 15:24:47 +0100

    fix get error

--- src/main/java/com/bugfree/beacon/BeaconController.java
+++ src/main/java/com/bugfree/beacon/BeaconController.java
@@ -5,6 +5,7 @@ import com.bugfree.json.JsonResponse;
 import com.bugfree.json.SuccessResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -34,9 +35,9 @@ public class BeaconController {
         return beaconDao.findByParametersList(b);
     }
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(value="/{id}", method = RequestMethod.GET)
     @ResponseBody
-    public JsonResponse get(String id) {
+    public JsonResponse get(@PathVariable String id) {
         Optional<Beacon> beacon = beaconDao.findOne(id);
         if (beacon.isPresent())
             return SuccessResponse.create(beacon.get());

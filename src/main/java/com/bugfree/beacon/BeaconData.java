e7ff0d3f6408ab310221fc99b9179c73ba3660cb
shark <szczerbicki.pawel@gmail.com>
2015-04-14 07:48:55 +0200

    remove supercar data

--- src/main/java/com/bugfree/beacon/BeaconData.java
+++ src/main/java/com/bugfree/beacon/BeaconData.java
@@ -2,44 +2,8 @@ package com.bugfree.beacon;
 
 public class BeaconData {
 
-    private String name;
-    private String carMake;
-    private String carModel;
-    private String milage;
     private String imageUrl;
     private String detailsUrl;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCarMake() {
-        return carMake;
-    }
-
-    public void setCarMake(String carMake) {
-        this.carMake = carMake;
-    }
-
-    public String getCarModel() {
-        return carModel;
-    }
-
-    public void setCarModel(String carModel) {
-        this.carModel = carModel;
-    }
-
-    public String getMilage() {
-        return milage;
-    }
-
-    public void setMilage(String milage) {
-        this.milage = milage;
-    }
 
     public String getImageUrl() {
         return imageUrl;

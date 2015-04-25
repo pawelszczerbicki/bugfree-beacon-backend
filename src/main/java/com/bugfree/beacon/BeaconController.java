6c3d0fddd8165f844162b3a3c6d66f1ff4cd80ea
shark <szczerbicki.pawel@gmail.com>
2015-04-26 01:15:30 +0200

    temporar test logger, add error reasons

--- src/main/java/com/bugfree/beacon/BeaconController.java
+++ src/main/java/com/bugfree/beacon/BeaconController.java
@@ -3,6 +3,7 @@ package com.bugfree.beacon;
 import com.bugfree.json.FailResponse;
 import com.bugfree.json.JsonResponse;
 import com.bugfree.json.SuccessResponse;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -11,13 +12,16 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 import java.util.Optional;
 
+import static com.bugfree.config.Keys.BEACON_NOT_PRESENT;
+import static com.bugfree.config.Keys.UPLOAD_ERROR;
+import static org.slf4j.LoggerFactory.getLogger;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 import static org.springframework.web.bind.annotation.RequestMethod.PUT;
 
 @Controller
 @RequestMapping("/beacon")
 public class BeaconController {
-
+    private static final Logger logger = getLogger(BeaconController.class);
     @Autowired
     private BeaconService service;
 
@@ -54,8 +58,17 @@ public class BeaconController {
     @ResponseBody
     public JsonResponse addPhoto(@PathVariable String id, MultipartFile file) {
         Optional<Beacon> b = service.findOne(id);
-        if (b.isPresent() && service.addPhoto(b.get(), file))
-            return SuccessResponse.create(b.get());
-        else return FailResponse.create();
+        if (!b.isPresent())
+            return FailResponse.create(BEACON_NOT_PRESENT);
+        else if (!service.addPhoto(b.get(), file))
+            return FailResponse.create(UPLOAD_ERROR);
+        else return SuccessResponse.create();
+    }
+
+    @RequestMapping("")
+    @ResponseBody
+    public String testLogger(){
+        logger.error("test logger");
+        return "test";
     }
 }
--- src/main/java/com/bugfree/config/Keys.java
+++ src/main/java/com/bugfree/config/Keys.java
@@ -6,4 +6,6 @@ package com.bugfree.config;
 public class Keys {
     public static final String PHOTOS_BUCKET = "amazon.s3.photo.bucket";
     public static final String S3_ENABLED = "amazon.s3.enabled";
+    public static final String BEACON_NOT_PRESENT = "Beacon with requested id is not present";
+    public static final String UPLOAD_ERROR = "Error occurred during photo upload";
 }

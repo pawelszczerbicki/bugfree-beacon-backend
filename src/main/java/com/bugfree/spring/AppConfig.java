cd8d218047091c054331c0c04b3881069073d887
shark <szczerbicki.pawel@gmail.com>
2015-08-11 21:35:07 +0200

    change mongo creation

--- src/main/java/com/bugfree/spring/AppConfig.java
+++ src/main/java/com/bugfree/spring/AppConfig.java
@@ -3,12 +3,13 @@ package com.bugfree.spring;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.bugfree.config.ConfigService;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.data.authentication.UserCredentials;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 import org.springframework.web.multipart.MultipartResolver;
@@ -17,6 +18,8 @@ import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import java.net.UnknownHostException;
 
 import static com.bugfree.config.Keys.*;
+import static com.mongodb.MongoCredential.createCredential;
+import static java.util.Collections.singletonList;
 
 /**
  * Created by pawel on 11.08.15.
@@ -36,10 +39,9 @@ public class AppConfig {
 
     @Bean
     public MongoTemplate mongoTemplate() throws UnknownHostException {
-        UserCredentials auth = new UserCredentials(config.property(MONGO_USERNAME), config.property(MONGO_PASSWORD));
-        MongoClient c = new MongoClient(config.property(MONGO_HOST), config.asInteger(MONGO_PORT));
-        //TODO change SimpleMongoDbFactory constructor. Current is deprecated
-        return new MongoTemplate(new SimpleMongoDbFactory(c, config.property(MONGO_DATABASE), auth));
+        MongoCredential credential = createCredential(config.property(MONGO_USERNAME), config.property(MONGO_DATABASE), config.property(MONGO_PASSWORD).toCharArray());
+        MongoClient c = new MongoClient(new ServerAddress(config.property(MONGO_HOST), config.asInteger(MONGO_PORT)), singletonList(credential));
+        return new MongoTemplate(new SimpleMongoDbFactory(c, config.property(MONGO_DATABASE)));
     }
 
     @Bean

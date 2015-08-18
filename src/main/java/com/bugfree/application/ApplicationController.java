fcd5104a9a4d8c190f6f7fb462cb26a3b0abbe10
shark <szczerbicki.pawel@gmail.com>
2015-08-18 23:46:00 +0200

    service get

--- src/main/java/com/bugfree/application/ApplicationController.java
+++ src/main/java/com/bugfree/application/ApplicationController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 import static org.springframework.web.bind.annotation.RequestMethod.PUT;
 
@@ -30,6 +31,12 @@ public class ApplicationController {
     @RequestMapping(method = {POST, PUT})
     public JsonResponse<Application> save(Application a) {
         Optional<Application> maybe = customerService.save(a);
+        return maybe.isPresent() ? SuccessResponse.create(maybe.get()) : FailResponse.create();
+    }
+
+    @RequestMapping(method = GET)
+    public JsonResponse<Application> get() {
+        Optional<Application> maybe = customerService.getApplication();
         return maybe.isPresent() ? SuccessResponse.create(maybe.get()) : FailResponse.create();
     }
 }

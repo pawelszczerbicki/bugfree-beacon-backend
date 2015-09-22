9d56ab8a18359b590de16d1f477d0bf31301f83e
shark <szczerbicki.pawel@gmail.com>
2015-09-22 22:45:09 +0200

    remove unused import

--- web/src/main/java/com/bugfree/web/translation/Translation.java
+++ web/src/main/java/com/bugfree/web/translation/Translation.java
@@ -1,6 +1,5 @@
 package com.bugfree.web.translation;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
